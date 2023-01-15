d31934caf691114c02e72032b975114d634a6b86
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-01-16 02:37:11 +1000

    Changing the arrangement of animations

---
+++ Weatherman/View/LottieView.swift
@@ -0,0 +1,37 @@
+//
+//  LottieView.swift
+//  Weatherman
+//
+//  Created by Илья Обухов on 16.01.2023.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<LottieView>) {
+        uiView.subviews.forEach({$0.removeFromSuperview()})
+        
+        let animationView = LottieAnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
+        ])
+        
+        animationView.animation = LottieAnimation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+    }
+}
