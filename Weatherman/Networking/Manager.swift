37b900397fc282221b752e476fc97dc2c5030d21
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-01-15 19:10:11 +1000

    Creating a manager to work with networks

---
+++ Weatherman/Networking/Manager.swift
@@ -0,0 +1,45 @@
+//
+//  Manager.swift
+//  Weatherman
+//
+//  Created by Илья Обухов on 15.01.2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invaidResponse
+    case invalidData
+    case error(err: String)
+    case decodingError(err: String)
+}
+
+
+class Manager<T: Codable> {
+    static func fetch(for url: URL, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                print(String(describing: error!))
+                completion(.failure(.error(err: error!.localizedDescription)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion(.failure(.invaidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let json = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(json))
+            } catch let err {
+                print(String(describing: err))
+                completion(.failure(.decodingError(err: err.localizedDescription)))
+            }
+        }.resume()
+}
