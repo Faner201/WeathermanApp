d85323a2b4cfd1e8ff53c2a51d07f8ff42f047fa
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-01-15 15:00:48 +1000

    git init

---
+++ Weatherman/WeathermanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathermanApp.swift
+//  Weatherman
+//
+//  Created by Илья Обухов on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeathermanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
